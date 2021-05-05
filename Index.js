cc8f5a96c8f24f293c4ceee622032c256c7b59ac
1rsd <rdiazro@uninpahu.edu.co>
2021-05-05 09:20:35 -0500

    se hacen modificacion al endpoint de upload your idea

--- Index.js
+++ Index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var mongoose= require('mongoose');
+var Schema= mongoose.Schema;
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(fileUpload());
+
+mongoose.connect("mongodb://localhost/photobook",{useNewUrlParser: true,useUnifiedTopology: true});
+var postSchemaJSON = {
+  post_name: String,
+  ubication: String,
+  size: String,
+  description: String
+};
+
+var post_schema= new Schema (postSchemaJSON);
+var post = mongoose.model("post",post_schema);
  
 app.get('/', function(req, res, next){
   console.log('get route', req.testing);
@@ -18,8 +33,7 @@ app.get('/', function(req, res, next){
 
   app.post('/Uploading_content', function(req, res, next){
     console.log('post content', req.body.testing);
-    res.send("Uploading content;Description: ; ");
-    res.end();
+    res.send("uploading_content");
   });
 
   app.get('/Post', function(req, res, next){
@@ -39,14 +53,16 @@ app.get('/', function(req, res, next){
     res.send("Post comment");
     res.end();
   });
-  app.get('/upload_your_idea', function(req, res, next){
-    console.log('get route', req.body.testing);
-    res.send("Redirection to upload your idea");
+  app.post('/upload_your_idea', function(req, res, next){
+    let EDFile = req.files.file
+    EDFile.mv(`./post/${EDFile.Photobook}`,err => {
+        if(err) return res.status(500).send({ message : err })
+        return res.status(200).send({ message : 'File upload' })
     res.end();
-  });
+  })
+});
 
 app.listen(3000,function(err){
     if (err)console.log("Problemas para abrir el puerto")
     console.log("Se abre puerto 3000")
 });
-
